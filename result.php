c81937d6a52d163a8299f0db111808e6f7115619
Aadhavan Srinivasan <aadhavan@twomorecents.org>
2023-06-06 23:53:32 -0500

    Added code to insert data into Cassandra DB

--- result.php
+++ result.php
@@ -5,15 +5,26 @@ use Casssandra;
 $cluster = Cassandra::cluster()->build();
 $keyspace = 'glink';
 
+$url = $_GET["url"];
+$shortlink = $_GET["glink"];
+
 $session = $cluster->connect($keyspace);
 
-$statement = new Cassandra\SimpleStatement('SELECT name FROM data WHERE id=5');
+//$statement = new Cassandra\SimpleStatement('SELECT name FROM data WHERE id=5');
+$rand_num = rand(0,999999);
 
-$future = $session->executeAsync($statement);
+$values = array(
+	'id' => $rand_num,
+	'url' => $url,
+	'shortlink' => $shortlink,
+);
+
+$statement = new Cassandra\SimpleStatement('INSERT INTO data (id, url, shortlink, when_created) VALUES (?,?,?,toTimestamp(now()))');
+$options = array('arguments' => $values);
+
+$future = $session->executeAsync($statement,$options);
 $result = $future->get();
 
-foreach ($result as $row) {
-	printf("The name is %s",$row['name']);
-}
+printf('Successful');
 
 ?>
