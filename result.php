92b70e632bab9c27e2966607f539801d7b016b3e
Aadhavan Srinivasan <aadhavan@twomorecents.org>
2023-06-08 01:32:30 -0500

    Added code to print out the inserted data, and to check if the data was inserted by querying the database for the inserted data

--- result.php
+++ result.php
@@ -11,20 +11,32 @@ $shortlink = $_GET["glink"];
 $session = $cluster->connect($keyspace);
 
 //$statement = new Cassandra\SimpleStatement('SELECT name FROM data WHERE id=5');
-$rand_num = rand(0,999999);
+$rand_num = rand(0,99999999);
 
 $values = array(
 	'id' => $rand_num,
 	'url' => $url,
 	'shortlink' => $shortlink,
 );
-
 $statement = new Cassandra\SimpleStatement('INSERT INTO data (id, url, shortlink, when_created) VALUES (?,?,?,toTimestamp(now()))');
 $options = array('arguments' => $values);
+$result = $session->execute($statement,$options);
 
-$future = $session->executeAsync($statement,$options);
-$result = $future->get();
+$statement = new Cassandra\SimpleStatement('SELECT url,shortlink FROM data WHERE id=?');
+$options = array('arguments' => array('id' => $rand_num));
+$result = $session->execute($statement,$options);
 
-printf('Successful');
+//$stringRepresentation= json_encode($result[0]);
+
+//printf("%s\n\n\n",$stringRepresentation);
+
+foreach($result as $row) {
+	if (is_null($row)) {
+		printf('Unsuccessful');
+	} else {
+		printf('Successful: The URL you entered was: %s and your GLink is: https://glink.zip/%s', $row['url'],$row['shortlink']);
+	}
+}
+//printf('Done');
 
 ?>
