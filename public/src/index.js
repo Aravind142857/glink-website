a8b8883b11297960768b81b3ff12fde4e268129e
Aravind142857 <aravind.s2025@berkeley.edu>
2023-06-09 20:08:47 -0500

    Node JS version

---
+++ public/src/index.js
@@ -0,0 +1,105 @@
+// const submit = document.getElementById("button");
+function getRandomGLink() {
+    //
+    return undefined;
+}
+
+// submit.addEventListener('click', validate);
+    function validate() {
+        //e.preventDefault();
+
+        const url = document.getElementById("URL");
+        let glink = document.getElementById("GLink");
+        const error = document.getElementById("error");
+        // if (!url) {
+        //     /* Flag */
+        // }
+        let valid = true;
+        const domainExp = new RegExp("^http(s)*:\\/\\/[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)+$");
+        const filepathExp = new RegExp("^[a-zA-Z]+$");
+        const glinkExp = new RegExp("^[a-zA-Z]*$");
+        let glinkStr = glink.value;
+        let count = 0;
+        let index = -1;
+        let domain = "";
+        let filepath = "";
+        for (let i = 0; i < url.value.length; i++) {
+            if (url.value.charAt(i) == '/') {
+                count++;
+            }
+            if (count == 3) {
+                index = i;
+                break;
+            }
+        }
+        if (count >= 3) {
+            domain = url.value.substring(0, index);
+            if (index == url.value.length - 1) {
+                filepath = url.value.charAt(index);
+            } else {
+                filepath = url.value.substring(index, url.value.length - 1);
+            }
+            alert("Domain is " + domain + " filepath is " + filepath);
+        } else {
+            domain = url.value;
+        }
+        console.log(domain);
+
+        if (glinkStr === "") {
+            var result = window.confirm("You have left the glink field blank. A random one will be generated for you.");
+            if (result === false) {
+                return false;
+            } else {
+                glinkStr = getRandomGLink();
+            }
+        }
+
+
+        if (domain.match(domainExp) && glinkStr.match(glinkExp))/** and is available? */{
+            if (error.classList.contains("visible")) {
+                error.classList.remove("visible");
+            }
+            if (url.classList.contains("invalid")) {
+                url.classList.remove("invalid");
+            }
+            url.classList.add("valid");
+            if (glink.classList.contains("invalid")) {
+                glink.classList.remove("invalid");
+            }
+            glink.classList.add("valid");
+            error.setAttribute('aria-hidden', true);
+            error.setAttribute('aria-invalid', false);
+            console.log("Valid with url= " +url.value+ " glink=" +glink);
+            return valid;
+        } else {
+            /*flag*/
+            error.classList.add("visible");
+
+            //error.classList.add("hidden");
+            if (!domain.match(domainExp)) {
+                if (url.classList.contains("valid")) {
+                    url.classList.remove("valid");
+                }
+                url.classList.add("invalid");
+            } else {
+                if (url.classList.contains("invalid")) {
+                    url.classList.remove("invalid");
+                }
+                url.classList.add("valid");
+            }
+            if (!glinkStr.match(glinkExp)) {
+                if (glink.classList.contains("valid")) {
+                    glink.classList.remove("valid");
+                }
+                glink.classList.add("invalid");
+            } else {
+                if (glink.classList.contains("invalid")) {
+                    glink.classList.remove("invalid");
+                }
+                glink.classList.add("valid");
+            }
+            error.setAttribute('aria-hidden', false);
+            error.setAttribute('aria-invalid', true);
+            return false;
+        }
+}
--- result.js
+++ result.js
@@ -1,8 +1,105 @@
+const path = require('path');
+//const http = require('http');
+const express = require('express');
+const app = express();
+const staticPath = path.join(__dirname, "/public");
+var bodyParser = require('body-parser')
 const cassandra = require('cassandra-driver');
-
 const client = new cassandra.Client({
 	contactPoints: ['127.0.0.1:9042'],
-	keyspace: 'glink',
+	localDataCenter: 'datacenter1',
+	keyspace: 'glink'
 });
+//var server = http.createServer(app);
+let id = 1;	/* Ideally should initialize id to be nextFromDB or write to file and read */
+const port = 63342;	// Port that the server listens on */
 
-const query = 
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static(staticPath));
+
+function nextId() {
+	let next = id;
+	id = id + 1;
+	return next;
+}
+function filter(path) {
+	if (path.match(new RegExp("^/[a-zA-Z]+/$"))) {
+		if (path.charAt(0) === '/') {
+			path = path.substring(1);
+		}
+		if (path.charAt(path.length - 1) === '/' && path.length > 1) {
+			path = path.substring(0, path.length - 1);
+		}
+		return path;
+	} else {
+		console.log("failed check");
+		return null;
+	}
+
+}
+app.get('/', (request, response) => {
+ 	response.render("index.html");
+ })
+app.get('/node_modules/', (request, response) => {
+	response.sendFile(path.join(staticPath, "/error.html"));
+})
+app.get('/public/', (request, response) => {
+	response.sendFile(path.join(staticPath, "/error.html"));
+})
+
+
+
+const query = "INSERT INTO data (id, url, glink) VALUES (?, ?, ?)";
+
+app.post('/add', function(req, res) {
+	let input_url = req.body.url;
+	let input_glink = req.body.glink;
+	console.log("Received query " + input_url + " and " + input_glink)
+	let currID = nextId();
+	let selectQuery = "SELECT id FROM data where glink = ? allow filtering";
+	client.execute(selectQuery, [input_glink],{} ,function(err, result) {
+		if (result.rows.length === 0) {
+			client.execute(query, [currID, input_url, input_glink], {prepare: true}, function(err,result) {
+				if (err) {
+					res.send("<html><body><p style=\"font-family:Futura; font-size:large; color:red;\">{err.message}</p></body></html>");
+				} else {
+					res.send("<html><body><p style=\"font-family:Futura; font-size:large; color:green;\">New entry has been added with url = " + req.body.url + " and glink = " + req.body.glink + "</p></body></html>");
+				}
+			});
+		} else {
+			res.send("<html><body><p style=\"font-family:futura; font-size:large; color:red;\">This glink has already been registered. Please try a different glink</p></body></html>");
+		}
+	});
+})
+
+/* Redirect requests to corresponding entry in database */
+app.get('/*', (request, response) => {
+	let req_path = request.path;
+	req_path = filter(req_path);
+	console.log("Path is " +req_path);
+	if (req_path == null) {
+		response.sendFile(path.join(staticPath, "/error.html"));
+		return;
+	}
+
+	let selQry = "select url from data where glink = ? allow filtering";
+	client.execute(selQry, [req_path], {}, function(err, result) {
+		if (result.rows.length === 0) {
+			console.log("Failed");
+			response.sendFile(path.join(staticPath, "/error.html"));
+		} else {
+			let page = result.rows[0]["url"];
+			console.log(page);
+			response.writeHead(301, {Location: page});
+			response.end();
+		}
+
+	})
+})
+app.listen(port, function(){
+	console.log("server listening on port 63342");
+})
+/** Validate url and glink on client side as well */
