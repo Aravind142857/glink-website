0e3a4f4a01a69f3b09aeb90b4dcdc44d23191c37
Aadhavan Srinivasan <aadhavan@twomorecents.org>
2023-09-06 21:58:24 -0500

    DON'T KNOW IF THIS WILL WORK: Added another database query to set 'hell' to true

--- deletelink.php
+++ deletelink.php
@@ -11,8 +11,11 @@ $row_id = $result[0]['id'];
 
 
 
-$statement = $session->prepare('UPDATE data USING TTL 8 SET hell=true, shortlink=?, url=?, user=?, latitude=?, longitude=?, radius=? WHERE id=?;');
+$statement = $session->prepare('UPDATE data USING TTL 8 SET shortlink=?, url=?, user=?, latitude=?, longitude=?, radius=? WHERE id=?;');
 $new_result = $session->execute($statement,array('arguments' => array($_POST['link'], $result[0]['url'], $result[0]['user'], $result[0]['latitude'], $result[0]['longitude'], $result[0]['radius'], $row_id)));
+
+$statement = $session->prepare('UPDATE data SET hell=true WHERE id=?;');
+$new_result = $session->execute($statement,array('arguments' => array($row_id)));
 
 
 http_response_code(204);
